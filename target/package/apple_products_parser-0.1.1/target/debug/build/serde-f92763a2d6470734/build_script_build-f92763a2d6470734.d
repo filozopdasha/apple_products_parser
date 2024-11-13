57e7828fb8ad282ee051a25818ebedfa1ae86ab8
filozopdasha <dariafilozop@gmail.com>
2024-11-13 23:38:06 +0200

    final commit

--- target/package/apple_products_parser-0.1.1/target/debug/build/serde-f92763a2d6470734/build_script_build-f92763a2d6470734.d
+++
@@ -1,5 +0,0 @@
-/Users/filozopdasha/Desktop/apple_products_parser/target/package/apple_products_parser-0.1.1/target/debug/build/serde-f92763a2d6470734/build_script_build-f92763a2d6470734: /Users/filozopdasha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.215/build.rs
-
-/Users/filozopdasha/Desktop/apple_products_parser/target/package/apple_products_parser-0.1.1/target/debug/build/serde-f92763a2d6470734/build_script_build-f92763a2d6470734.d: /Users/filozopdasha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.215/build.rs
-
-/Users/filozopdasha/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.215/build.rs:
